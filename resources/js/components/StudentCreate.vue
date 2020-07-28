cffce541483b0e52be71b631f205dd6efd8d4e99
Mounir El imlahi <munir.elimlahi86@gmail.com>
2020-07-28 16:10:24 +0200

    Fixed Update Student vue Component

---
+++ resources/js/components/StudentCreate.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <div class="card-header">
+            Añadir Alumno
+        </div>
+        <div class="card-body">
+                <div class="form-group">
+                    <label>Nombre</label>
+                    <input type="text" v-model="student.name" name="name" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label>Apellido</label>
+                    <input type="text" v-model="student.surname" name="surname" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label>Nacionalidad</label>
+                    <input type="text" v-model="student.nationality" name="nationality" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" v-model="student.email" name="email" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label>Sexo</label>
+                    <input type="text" v-model="student.gender" name="gender" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label>Curso Actual</label>
+                    <input type="text" v-model="student.currentcourse" name="currentcourse" class="form-control" required>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-primary" @click="createStudent(student)" value="Crear">Crear</button>
+                </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+        student: {}
+    };
+  },
+  mounted() {
+
+        console.log("Component mounted.");
+  },
+    methods:{
+
+        createStudent(student){
+                axios.post("/api/students/", this.student).then(
+                    window.location.replace('/students')
+                );
+
+        }
+
+    }
+};
+</script>
--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -1,40 +1,5 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="container">
-    <div class="card-header">Nuevo</div>
-        <div class="card-body">
-            <form action="{{Route('student.store')}}" method="POST">
-                @csrf
-                <div class="form-group">
-                    <div class="form-group">
-                        <label>Nombre</label>
-                        <input type="text" name="name" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Apellido</label>
-                        <input type="text" name="surname" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Nacionalidad</label>
-                        <input type="text" name="nationality" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Email</label>
-                        <input type="email" name="email" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Sexo</label>
-                        <input type="text" name="gender" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Curso Actual</label>
-                        <input type="text" name="currentcourse" class="form-control" required>
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <input type="submit" value="Crear" class = "btn btn-primary">
-                </div>
-        </form>
-</div>
+    <student-create></student-create>
 @endsection
