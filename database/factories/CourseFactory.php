71a7d5896dd7832c34248de41a40ad596dbe0062
rapidlizard <kiko.cbg@gmail.com>
2020-07-28 09:46:51 +0200

    change course factory

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Course::class, function (Faker $faker) {
     return [
-        'title' => $faker->title,
+        'title' => $faker->bs,
         'description' => $faker->text,
         'start_date' => $faker->date,
         'end_date' => $faker->date,
