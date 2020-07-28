98d9d2bf0b6f92adcf637cbf0b2622a825cf2e0a
Sebastian Herrera <29juansebastian@gmail.com>
2020-07-28 10:15:50 +0100

    crate teacher components

---
+++ resources/js/components/TeacherIndex.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div class="card-header">
+      <a href="/teacher/create" class="btn btn-primary">Añadir profesor</a>
+    </div>
+    <div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Nombre</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-bind:key="i" v-for="(teacher, i) in teachers">
+            <td>
+              <a :href="'teache/' + teacher.id">{{teacher.name}}</a>
+            </td>
+            <td>
+              <a :href="'teacher/' + teacher.id + '/edit'" class="btn btn-success">Modificar</a>
+            </td>
+            <td>
+              <button @click="deleteTeacher(teacher)">Eliminar</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      teachers: [],
+    };
+  },
+  methods: {
+    getTeachers() {
+      axios.get("/api/teachers").then((response) => {
+        this.teachers = response.data;
+      });
+    },
+    deleteTeacher(teacher) {
+      axios.delete("/api/teachers/" + teacher.id).then((response) => {
+        this.teachers = response.data;
+      });
+    },
+  },
+  mounted() {
+    this.getTeachers();
+    console.log("Component mounted.");
+  },
+};
+</script>
