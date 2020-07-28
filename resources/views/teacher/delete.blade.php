de424b925c37b1746f02eda3ffe5a3fd86439328
JoseOrtegaVillen <joseortegaaula@gmail.com>
2020-07-28 16:11:20 +0200

    add controller and view for delete teacher

--- resources/views/teacher/delete.blade.php
+++ resources/views/teacher/delete.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.dashboard')
 
 @section('content')
+<div class="container">
+    <div class="card-header">Nuevo</div>
+        <div class="card-body">
+            <form action="{{Route('teacher.destroy')}}" method="POST">
+                @csrf
+                @method('delete')
 
+            </div>
+                <div class="card-footer">
+                <input type="submit" value="Crear" class = "btn btn-primary">
+            </div>
+        </form>
+</div>
 @endsection
