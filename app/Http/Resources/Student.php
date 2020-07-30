6b2602cc261a8bd73f5a3bc4dd10d2846a8e97f9
rapidlizard <kiko.cbg@gmail.com>
2020-07-30 12:35:06 +0200

    add course to student resource

--- app/Http/Resources/Student.php
+++ app/Http/Resources/Student.php
@@ -2,26 +2,21 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\JsonResource;
 use App\Course;
+use Illuminate\Http\Resources\Json\JsonResource;
+
 class Student extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
     public function toArray($request)
     {
         return [
-            'id'=>$this->id,
-            'name'=>$this->name,
-            'surname'=>$this->surname,
-            'nationality'=>$this->nationality,
-            'email'=>$this->email,
-            'gender'=>$this->gender,
-
+            'id' => $this->id,
+            'name' => $this->name,
+            'surname' => $this->surname,
+            'nationality' => $this->nationality,
+            'email' => $this->email,
+            'gender' => $this->gender,
+            'course' => $this->course,
         ];
     }
 }
