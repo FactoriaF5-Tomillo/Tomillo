74730e84d96c9eb80611ccc9236589dccb2619fb
rapidlizard <kiko.cbg@gmail.com>
2020-08-20 14:48:53 +0200

    change admin, student and teacher seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,9 +12,31 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class)->create(['name'=>'Mounir','surname'=> 'El imlahi','email'=>'mounir@factoriaf5.com','type'=>'Admin', 'gender'=>'Hombre', 'nationality'=> 'Morocco','password' => Hash::make('password')]);
-        factory(User::class)->create(['name'=>'Umit','surname'=> 'Can Batur','email'=>'umit@factoriaf5.com','type'=>'Teacher', 'gender'=>'Hombre', 'nationality'=> 'Turkey','password' => Hash::make('password')]);
-        factory(User::class)->create(['name'=>'Francisco','surname'=> 'Goncalves','email'=>'francisco@factoriaf5.com','type'=>'Student', 'gender'=>'Hombre', 'nationality'=> 'Portugal','password' => Hash::make('password')]);
+        factory(User::class)->create([
+            'name'=>'Mounir',
+            'surname'=> 'El imlahi',
+            'email'=>'admin@tomillo.com',
+            'type'=>'Admin', 'gender'=>'Hombre', 
+            'nationality'=> 'Morocco',
+            'password' => Hash::make('password')
+        ]);
+        factory(User::class)->create([
+            'name'=>'Umit',
+            'surname'=> 'Can Batur',
+            'email'=>'teacher@tomillo.com',
+            'type'=>'Teacher', 'gender'=>'Hombre',
+            'nationality'=> 'Turkey',
+            'password' => Hash::make('password')
+        ]);
+        factory(User::class)->create([
+            'name'=>'Francisco'
+            ,'surname'=> 'Goncalves'
+            ,'email'=>'student@tomillo.com'
+            ,'type'=>'Student'
+            , 'gender'=>'Hombre'
+            ,'nationality'=> 'Portugal'
+            ,'password' => Hash::make('password')
+        ]);
         factory(User::class, 25)->create();
     }
 }
